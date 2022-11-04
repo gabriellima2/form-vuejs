5086cb31bab7de8a2b7f8fa013052b7613dcbbbb
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-11-04 15:18:15 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>Hello Form!</h1>
+</template>
+
+<style lang="scss">
+@import "./assets/scss/main";
+* {
+	box-sizing: border-box;
+	padding: 0;
+	margin: 0;
+}
+
+body {
+	color: $font-color;
+	font-family: $font;
+	background-color: $background-color;
+}
+
+li {
+	list-style: none;
+}
+</style>
